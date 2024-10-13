a2733399797792e81ef8bdf163c59570c810eb5d
gilspi <pashabicepsv_p@mail.ru>
2024-10-13 13:49:40 +0300

    Change logics, buttons

--- serial.c
+++ serial.c
@@ -1,102 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>  // для взаимодействия с COM портами
-#include <string.h>
-#include <process.h>  // потоки
+#include <windows.h>
+#include <process.h>
+#include "serial.h"
 
+void configureComPort(HANDLE hComm, DWORD baudrate,
+                      DWORD bytesize, DWORD stopbits,
+                      DWORD parity) {
+    /*
+    Конфигурация порта при запуске приложения.
+    baudrate - скорость передачи данных (например, 115200).
+    bytesize - размер данных (например, 8 бит).
+    stopbits - количество стоп-битов (ONESTOPBIT или TWOSTOPBITS).
+    parity - проверка четности (NOPARITY, EVENPARITY, ODDPARITY).
+    */
+    
+    DCB dcb;  // работа с параметрами COM-порта
+    SecureZeroMemory(&dcb, sizeof(DCB));  // заполнение области памяти нулями
 
-HANDLE hSerial;  // дескриптор COM порта
-
-
-unsigned __stdcall write_to_com(void *arg) {
-    (void) arg;
-    char *data = "<MAonce>";  // данные для отправки
-    DWORD bytes_written;  // uint32_t
-
-    while(1) {
-        if (!WriteFile(hSerial, data, strlen(data), &bytes_written, NULL)) {
-            perror("Ошибка при записи в COM-порт\n");
-            exit(EXIT_FAILURE);
-        } else {
-            printf("ДАнные записаны в COM порт\n");
-        }
-
-        printf("Отправлено: %s\n", data);
-        Sleep(2000);  // задержка между отправками
-    }
-
-    return 0;
-}
-
-
-unsigned __stdcall read_from_port(void *arg) {
-    (void) arg;
-    /*__stdcall управляет под капотом, как передаются аргументы
-    и очищаются стек. Стандарт для взаимодействия с системными функциями
-    и библиотеками.*/
-    char buffer[256];
-    DWORD bytesRead;  // uint32_t
-    while(1) {
-        memset(buffer, 0, sizeof(buffer));
-
-        if (ReadFile(hSerial, buffer, sizeof(buffer) - 1, &bytesRead, NULL)) {
-            if (bytesRead > 0) {
-                buffer[bytesRead] = '\0';  // конец строки
-                printf("Прочитано %s\n", buffer);
-            }
-        } else {
-            perror("Ошибка при чтении с COM-порта\n");
-            exit(EXIT_FAILURE);
-        }
-
-        Sleep(500);  // ждем новые данные
-    }
-
-    return 0;
-}
-
-
-int main() {
-    hSerial = CreateFile(
-        "\\\\.\\COM3",
-        GENERIC_READ | GENERIC_WRITE,
-        0,
-        NULL,
-        OPEN_EXISTING,
-        0,
-        NULL
-    );
-
-    if (hSerial == INVALID_HANDLE_VALUE) {
-        perror("Ошибка при открытии порта...\n");
+    dcb.DCBlength = sizeof(DCB);
+    if (!GetCommState(hComm, &dcb)) {
+        perror("Не удалось получить параметры COM порта\n");
+        CloseHandle(hComm);
         exit(EXIT_FAILURE);
     } else {
-        printf("Соединение установлено!\n");
+        printf("Параметры COM порта успешно получены\n");
     }
 
-    DCB dcbSerialParams = {0};
-    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    dcb.BaudRate = baudrate;  // Скорость порта
+    dcb.ByteSize = bytesize;  // 8 бит данных
+    dcb.StopBits = stopbits;  // Один стоп-бит
+    dcb.Parity = parity;    // Без четности
 
-    if (!GetCommState(hSerial, &dcbSerialParams)) {
-        perror("Не удалось получить параметры порта...\n");
-        CloseHandle(hSerial);
+    if (!SetCommState(hComm, &dcb)) {
+        perror("Не удалось установить параметры COM порта\n");
+        CloseHandle(hComm);
         exit(EXIT_FAILURE);
     } else {
-        printf("Параметры порта получены.\n");
-    }
-
-    dcbSerialParams.BaudRate = CBR_115200;  // скорость 115200
-    dcbSerialParams.ByteSize = 8;  // 8 бит данных
-    dcbSerialParams.StopBits = ONESTOPBIT;  // один стоп-бит
-    dcbSerialParams.Parity = NOPARITY;  // без четности
-
-    // применяем новые настройки порта 
-    if (!SetCommState(hSerial, &dcbSerialParams)) {
-        perror("Не удалось установить параметры порта\n");
-        CloseHandle(hSerial);
-        exit(EXIT_FAILURE);
-    } else {
-        printf("Параметры для порта - установлены\n");
+        printf("Параметры COM порта успешно установлены\n");
     }
 
     COMMTIMEOUTS timeouts = {0};
@@ -104,21 +45,58 @@ int main() {
     timeouts.ReadTotalTimeoutConstant = 50;  // общий таймаут для операций чтения
     timeouts.WriteTotalTimeoutMultiplier = 10;  // таймаут на каждый байт
 
-    if (!SetCommTimeouts(hSerial, &timeouts)) {
+    if (!SetCommTimeouts(hComm, &timeouts)) {
         perror("Не удалось установить таймауты порта\n");
+        CloseHandle(hComm);
         exit(EXIT_FAILURE);
     } else {
         printf("Таймауты установлены!");
     }
 
-    _beginthreadex(NULL, 0, &write_to_com, NULL, 0, NULL);  // создание нового потока на WIN для отправки
-    _beginthreadex(NULL, 0, &read_from_port, NULL, 0, NULL);
+}
 
-    while(1) {
-        Sleep(1000);  // просто ждем окончания
+void writeToComPort(HANDLE hComm, const char *data) {
+    /*
+    Функция отправляет команду на порт только один раз после изменения
+    состояния кнопки на графическом пользовательском интерфейсе.
+    */
+
+    DWORD bytesWritten;  // uint32_t
+    if (!WriteFile(hComm, data, strlen(data), &bytesWritten, NULL)) {
+        perror("Ошибка при записи в COM порт\n");
+        CloseHandle(hComm);
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Данные отправлены: %s\n", data);
     }
+}
 
-    CloseHandle(hSerial);
+unsigned __stdcall read_from_port(void *arg) {
+    /*
+    __stdcall управляет под капотом, как передаются аргументы
+    и очищаются стек. Стандарт для взаимодействия с системными функциями
+    и библиотеками.
+    */
+
+    HANDLE hComm = (HANDLE)arg;  // Преобразуем аргумент обратно в HANDLE
+    (void) arg;
+    char buffer[256];
+    DWORD bytesRead;
+
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));  // каждый раз чистим область памяти
+
+        if (ReadFile(hComm, buffer, sizeof(buffer) - 1, &bytesRead, NULL)) {
+            if (bytesRead > 0) {
+                buffer[bytesRead] = '\0';  // завершаем строку
+                printf("Прочитано: %s\n", buffer);  // выводим данные
+            }
+        } else {
+            printf("Ошибка при чтении с COM порта\n");
+        }
+
+        Sleep(500);  // ждем следующие данные
+    }
 
     return 0;
 }
